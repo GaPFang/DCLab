de6b52c90fbc54b4cdb38f88967da0ffd0927e84
jialin <jlfang0119@gmail.com>
2024-10-01 17:27:57 +0800

    FIX spyglass Montgomery

--- Lab2/Lab2/src/Montgomery.sv
+++ Lab2/Lab2/src/Montgomery.sv
@@ -6,12 +6,15 @@ module Montgomery (
     output [255:0] o_montgomery,
     output         o_finished
 );
-    localparam S_IDLE = 1'b0;
-    localparam S_CALC = 1'b1;
+    typedef enum logic {
+        S_IDLE,
+        S_CALC
+    } state_t;
+
     localparam m_size = 4;
     localparam cycles = 256 / m_size;
 
-    logic state, state_nxt;
+    state_t state, state_nxt;
     logic [257:0] tmp1 [0:m_size-1];
     logic [257:0] tmp2 [0:m_size-1];
     logic [257:0] tmp3 [0:m_size-1];
@@ -36,6 +39,7 @@ module Montgomery (
             tmp2[i] = 0;
             tmp3[i] = 0;
         end
+        m_w = 0;
         case(state)
             S_IDLE: begin
                 if(i_start) begin
@@ -48,12 +52,12 @@ module Montgomery (
                 filter_nxt = filter << m_size;
                 if(cycle < cycles) begin
                     state_nxt = S_CALC;
-                    tmp1[0] = (a & filter) ? m_r + b : m_r;
-                    tmp2[0] = (tmp1[0] & 1'b1) ? tmp1[0] + N : tmp1[0];
+                    tmp1[0] = (| (a & filter)) ? m_r + b : m_r;
+                    tmp2[0] = (| (tmp1[0] & 1'b1)) ? tmp1[0] + N : tmp1[0];
                     tmp3[0] = tmp2[0] >> 1;
                     for (i = 1; i < m_size; i = i + 1) begin
-                        tmp1[i] = (a & (filter << i)) ? tmp3[i - 1] + b : tmp3[i - 1];
-                        tmp2[i] = (tmp1[i] & 1'b1) ? tmp1[i] + N : tmp1[i];
+                        tmp1[i] = (| (a & (filter << i))) ? tmp3[i - 1] + b : tmp3[i - 1];
+                        tmp2[i] = (| (tmp1[i] & 1'b1)) ? tmp1[i] + N : tmp1[i];
                         tmp3[i] = tmp2[i] >> 1;
                     end
                     m_w = tmp3[m_size - 1];
@@ -67,15 +71,18 @@ module Montgomery (
 
     always_ff @(posedge i_clk or negedge i_rst) begin
         if (~i_rst) begin
-            m_r = 0;
+            m_r <= 0;
             state <= S_IDLE;
             cycle <= 0;
             o_finished_r <= 0;
             o_montgomery_r <= 0;
             filter <= 1;
+            a <= 0;
+            b <= 0;
+            N <= 0;
         end else begin
             o_finished_r <= 0;
-            o_montgomery_r <= (m_r >= N) ? m_r - N : m_r;
+            o_montgomery_r <= (m_r >= {2'b0, N}) ? m_r - N : m_r;
             state <= state_nxt;
             cycle <= cycle_nxt;
             filter <= filter_nxt;
@@ -84,7 +91,6 @@ module Montgomery (
                 a <= i_a;
                 b <= i_b;
                 m_r <= 0;
-                cycle <= 0;
             end else if (state == S_CALC && state_nxt == S_CALC) begin
                 m_r <= m_w;
             end else if (state == S_CALC && state_nxt == S_IDLE) begin
